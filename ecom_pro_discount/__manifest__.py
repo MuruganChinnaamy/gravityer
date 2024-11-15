1bfbc55d10e8f71b981dfe079a0b1ee10dfeab55
Murugan Chinnasamy <30517341+MuruganChinnaamy@users.noreply.github.com>
2024-11-15 20:34:43 +0530

    Add files via upload
    task_assignment

---
+++ ecom_pro_discount/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Discount(Ecommerce)',
+    'version': '17.0.1.0.0',
+    'author': 'Murugan',
+    'depends': ['website_sale','account'],
+    'data': [
+        'views/website_sale_templates.xml',
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
